836f93d6c05a56c4db12db16144ddbeec622be4d
Tosin Olawore <25176099+tosinolawore@users.noreply.github.com>
2021-10-02 23:33:09 +0100

    Refactor the test cases with pytest fixtures

--- celeryapi/tests.py
+++ celeryapi/tests.py
@@ -10,6 +10,13 @@ def api_client():
    from rest_framework.test import APIClient
    return APIClient()
 
+@pytest.fixture
+def test_task():
+    task = Task(name='any string',)
+    task.save()
+    return task
+
+
 # Tests that the Task Model can create objects.
 @pytest.mark.django_db
 def test_task_count():
@@ -31,29 +38,28 @@ def test_api_start_task(api_client):
 
 # Tests API GET View.
 @pytest.mark.django_db
-def test_api_start_task(api_client):
+def test_api_get_task(api_client,test_task):
     """Tests the GET VIEW."""
 
-    task = Task.objects.get()
-    response = api_client.get('/tasks/', kwargs={'pk': task.id}, format="json")
+    response = api_client.get('/tasks/', kwargs={'pk': test_task.id}, format="json")
 
     assert response.status_code, status.HTTP_200_OK
-    assert response, task
 
 
 # Tests API UPDATE View.
 @pytest.mark.django_db
-def test_api_start_task(api_client):
+def test_api_update_task(api_client, test_task):
     """Tests the UPDATE VIEW."""
 
-    response = api_client.post(reverse('task_details'), {"name":"some string"}, format="json")
-    assert response.status_code == status.HTTP_201_CREATED
+    response = api_client.put(reverse('task_details',kwargs={'pk': test_task.id}),
+            {"name":"new string"}, format="json")
+    assert response.status_code == status.HTTP_200_OK
 
 
 # Tests API DELETE View.
 @pytest.mark.django_db
-def test_api_start_task(api_client):
+def test_api_delete_task(api_client,test_task):
     """Tests the DELETE VIEW."""
 
-    response = api_client.post(reverse('create_task'), {"name":"some string"}, format="json")
-    assert response.status_code == status.HTTP_201_CREATED
+    response = api_client.delete(reverse('task_details',kwargs={'pk': test_task.id}), format="json", follow=True)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
